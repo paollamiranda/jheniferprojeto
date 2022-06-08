c23297c22d152d837e895a96344e659cf521d041
jhenifer <jhenifermachado211@gmail.com>
2022-06-08 15:00:01 -0300

    enviar

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import HeaderComp from "@/components/Template/HeaderComp.vue";
+import FooterComp from "@/components/Template/FooterComp.vue";
+</script>
+
+<template>
+  <HeaderComp />
+  <main>
+  <RouterView />
+  </main>
+  <FooterComp />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  height: 100vh;
+  /* padding: 2rem; */
+
+  font-weight: normal;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+footer {
+  height: 10%;
+  background-color: #db7093;
+  color: black;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+}
+header span {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+main {
+  height: 80%;
+}
+</style>
